0ba92cc030bf28e801ada84c75fd5e56df472cbf
stelinmariacj <72865736+stelinmariacj@users.noreply.github.com>
2021-11-15 18:05:12 +0530

    setup forecast error

--- src/app.js
+++ src/app.js
@@ -83,7 +83,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Stelin maria',
         errorMessage: 'Help article not found.'
     })
 })
@@ -91,7 +91,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'stelin maria',
         errorMessage: 'Page not found.'
     })
 })
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,17 +1,18 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.weatherstack/forecast?access_key=35cd0e9c24be438d6c4b08ffdd104de0/' + latitude + ',' + longitude
+    const url = 'http://api.weatherstack.com/current?access_key=35cd0e9c24be438d6c4b08ffdd104de0&query=' + latitude + ',' + longitude + '&units=f'
 
-    request({ url, json: true }, (error, { body }) => {
+    request({ url, json: true }, (error, {body}) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out.")
         }
     })
 }
 
 module.exports = forecast
+
