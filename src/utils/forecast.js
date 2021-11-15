84c30e7a941c8b6046aa537d536ab4f540588572
stelinmariacj <72865736+stelinmariacj@users.noreply.github.com>
2021-11-15 18:42:34 +0530

    add data to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,18 +1,17 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
+    
     const url = 'http://api.weatherstack.com/current?access_key=35cd0e9c24be438d6c4b08ffdd104de0&query=' + latitude + ',' + longitude + '&units=f'
-
-    request({ url, json: true }, (error, {body}) => {
+    request({ url: url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out.")
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out. It feels like " + body.current.feelslike + " degress out. The humidity is " + body.current.humidity + "%.")
         }
     })
 }
 
 module.exports = forecast
-
